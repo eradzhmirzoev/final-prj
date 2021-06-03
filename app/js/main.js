c7ff6ccb23f168655034515420fe674e521b1e7c
Eradzh Mirzoiev <eradzh.m@gmail.com>
2021-06-04 02:43:04 +0300

    06.06.2021

--- app/js/main.js
+++ app/js/main.js
@@ -15,3 +15,26 @@ window.onscroll = function() {
     }
 
 }
+
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        adaptiveHeight: true,
+        speed: 1500,
+        autoplay: false,
+        autoplaySpeed: 4000,
+        variableWidth: true,
+        responsive:[
+            {
+                breakpoint: 577,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                }
+            }
+        ]
+    });
+});
